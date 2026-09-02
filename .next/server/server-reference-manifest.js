4bfc2c6297d817a2f297743366c5e7daca265e46
Eslam Hany <https://github.com/eslamhany71-lgtm>
2026-09-03 00:35:50 +0300

    Fix build errors and remove .next cache

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"+mm7CC+iEOdqcZY4OoJ6UXqnBGY9VWDL9EECyPC1qXI=\"\n}"
--- app/api/careers/route.ts
+++ app/api/careers/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 import nodemailer from "nodemailer";
+export const dynamic = "force-dynamic";
 
 const prisma = new PrismaClient();
 
